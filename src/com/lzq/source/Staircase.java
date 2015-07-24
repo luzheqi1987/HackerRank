12f7b3b9d971165fa9a00d24bc3603b8a7dfeab9
luzq <luzq@sevenga.com>
2015-07-24 19:06:17 +0800

    new

---
+++ src/com/lzq/source/Staircase.java
@@ -0,0 +1,22 @@
+package com.lzq.source;
+
+import java.util.Scanner;
+
+public class Staircase {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            for(int j = 0; j < t - i - 1; j++){
+                System.out.print(' ');
+            }
+            for(int j = 0; j <= i; j++){
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+
+}
